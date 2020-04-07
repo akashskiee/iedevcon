592712a1dd613f1a831fe4308b37aba9a14c4671
Akash Kumar <akashpk1997@gmail.com>
2020-04-07 11:40:05 +0530

    (feat) Add delete experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -185,6 +185,24 @@ router.put('/experience', [auth, [
         console.error(err.message);
         res.status(500).send('Server Error');
     }
-})
+});
+
+//DELETE profile experience /profile/experience/:exp_id
+
+router.delete('/experience/:exp_id', auth , async (req, res) => {
+    try{
+        const profile = await Profile.findOne({ user: req.user.id})
+
+        const removeIndex = profile.experience.map(item => item.id).indexOf
+        (req.params.exp_id);
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+        res.json(profile);
+    } catch (err) {
+        console.error(err.mesage);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
