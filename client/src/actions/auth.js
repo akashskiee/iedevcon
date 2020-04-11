14eed65331e763fc3c5e2648d4cafdad6fff1f4b
Akash Kumar <akashpk1997@gmail.com>
2020-04-12 01:34:49 +0530

    Add login functionality, reducer and action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR} from './types';
+import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL} from './types';
 import setAuthToken from '../utils/setAuthToken';
 import {setAlert} from './alert';
 
@@ -53,4 +53,36 @@ export const register = ({ name, email, password}) => async dispatch => {
             type: REGISTER_FAIL
         });
     }
-}
+};
+
+
+//Login user
+
+export const login = ( email, password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type' : 'application/json' 
+        }
+    }
+    const body = JSON.stringify({ email, password});
+
+    try {
+        const res = await axios.post('/api/auth', body, config);
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+
+        dispatch(loadUser());
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if(errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+        dispatch({
+            type: LOGIN_FAIL
+        });
+    }
+};
