48214e8c90fc7e7a535f307badda4ad0b8bc7676
Akash Kumar <akashpk1997@gmail.com>
2020-04-12 01:41:46 +0530

    (feat) Add Logout type and reducer

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
-import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL} from './types';
+import { 
+    REGISTER_FAIL,
+    REGISTER_SUCCESS,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT
+} from './types';
 import setAuthToken from '../utils/setAuthToken';
 import {setAlert} from './alert';
 
@@ -86,3 +94,11 @@ export const login = ( email, password) => async dispatch => {
         });
     }
 };
+
+//Logout
+
+export const logout = () => dispatch => {
+    dispatch({
+        type: LOGOUT
+    })
+}
