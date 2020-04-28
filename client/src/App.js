3db1285e93f66c6c44ff3909a804efd69bf03382
Akash Kumar <akashpk1997@gmail.com>
2020-04-29 01:09:25 +0530

    (feat) Add experience and education

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import setAuthToken from './utils/setAuthToken';
 import {loadUser} from './actions/auth';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-form/CreateProfile';
+import AddEducation from './components/profile-form/AddEducation';
+import AddExperience from './components/profile-form/AddExperience';
 import EditProfile from './components/profile-form/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 
@@ -36,7 +38,8 @@ const App = () => {
             <Route exact path="/login" component={Login} />
             <PrivateRoute exact path="/dashboard" component={Dashboard} />
             <PrivateRoute exact path="/create-profile" component={CreateProfile} />
-            <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+            <PrivateRoute exact path="/add-experience" component={AddExperience} />
+            <PrivateRoute exact path="/add-education" component={AddEducation} />
         </Switch>
      </section>
      </Fragment>
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {setAlert} from './alert';
-import {GET_PROFILE, PROFILE_ERROR} from './types';
+import {GET_PROFILE, UPDATE_PROFILE, PROFILE_ERROR} from './types';
 
 
 //Get current user's profile
@@ -55,4 +55,71 @@ export const createProfile = (formData, history, edit=false) => async dispatch =
             payload: {msg: err.resopnse, status: err.response}
         });
     }
-}
+};
+
+//Add experience
+
+export const addExperience = (formData, history) => async dispatch => {
+    try {
+        const config = {
+            headers : {
+                'Content-Type' : 'application/json'
+            }
+        };
+
+        const res = await axios.put('/api/profile/experience', formData, config);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+        dispatch(setAlert('Experience added!', 'success'));
+        history.push('/dashboard')
+
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if(errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.resopnse, status: err.response}
+        });
+    }
+};
+
+//Add education
+
+export const addEducation = (formData, history) => async dispatch => {
+    try {
+        const config = {
+            headers : {
+                'Content-Type' : 'application/json'
+            }
+        };
+
+        const res = await axios.put('/api/profile/education', formData, config);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+        dispatch(setAlert('Education added!', 'success'));
+        history.push('/dashboard')
+
+    } catch (err) {
+        const errors = err.response.data.errors;
+
+        if(errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {msg: err.resopnse, status: err.response}
+        });
+    }
+};
+
+
+
