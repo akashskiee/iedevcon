f984afd72121655b80caad654c6346fa324a37db
Akash Kumar <akashpk1997@gmail.com>
2020-04-29 01:37:36 +0530

    (feat) Add education and experience display to dashboard

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -3,7 +3,9 @@ import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {getCurrentProfile} from '../../actions/profile';
-import DashboardActions from './DashboardActions'
+import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education'
 import Spinner from '../layout/Spinner';
 
 const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile, loading}}) => {
@@ -17,7 +19,12 @@ const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile, loading}
     <p className="lead">
         <i className="fas fa-user"> Welcome {user && user.name}</i>
     </p>
-    {profile !== null ? (<Fragment><DashboardActions /></Fragment>) : 
+    {profile !== null ? (
+        <Fragment>
+            <DashboardActions />
+            <Experience experience={profile.experience} />
+            <Education education={profile.education} />
+        </Fragment>) : 
     (<Fragment>
     <p>You have not set up a profile... please add some</p>
     <Link to='/create-profile' className="btn btn-primary my-1" >Create Profile</Link>
