10be26c6854a82fd5fe8aa0aa93a4eb2ccdabe82
Akash Kumar <akashpk1997@gmail.com>
2020-04-30 00:34:11 +0530

    (bug) Fix react hook console error

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -11,7 +11,7 @@ import Spinner from '../layout/Spinner';
 const Dashboard = ({getCurrentProfile, deleteAccount, auth: {user}, profile: {profile, loading}}) => {
     useEffect(() => {
         getCurrentProfile();
-    }, []);
+    }, [getCurrentProfile]);
     return loading && profile === null ? <Spinner /> : <Fragment>
     <h1 className="large text-primary">
     Dashboard
